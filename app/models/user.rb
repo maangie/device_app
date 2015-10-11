a8ea7d1d58fea2a56c0f17ec9f82e4278c39ea69
maangie <maangie906@gmail.com>
2015-10-11 14:12:33 +0900

    authenticate with twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# User
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -5,4 +6,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable, :omniauthable,
          omniauth_providers: [:twitter]
+
+  def self.from_omniauth(auth)
+    provider = auth['provider']
+    uid = auth['uid']
+    where(provider: provider, uid: uid).first_or_create do |user|
+      user.provider = provider
+      user.uid = uid
+      user.username = auth['info']['nickname']
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session['devise.user_attributes']
+      new(session['devise.user_attributes'], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
 end
